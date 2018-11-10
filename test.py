e8527ac197f24c2b80267471accaab030ba9b51e
wioletta.kolasa <wioletta.kolasa@gmail.com>
2018-11-10 17:57:19 +0100

    initial project version

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 10 17:30:55 2018
+
+@author: Wioletta
+"""
+import cv2
+
+img = cv2.imread('CroppedYale\CroppedYale\yaleB01\yaleB01_P00A+000E+00.pgm')
+
+cv2.imshow('Image',img)
